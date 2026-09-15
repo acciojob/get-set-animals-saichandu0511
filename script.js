2866d50e6d1c578e3725ec8991fca00ead931389
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-15 20:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ class Animal {
 		return this.species;
 	}
 	makesound(){
-		 console.log('The ${this.species} makes a sound');
+		 console.log('The+{this.species} "makes a sound");
 	}
 }
 
 class Dog extends Animal {
-	purr(){
-		console.log("purr")
+	bark(){
+		console.log("woof")
 	}
 }
 
 class Cat extends Animal {
-	bark(){
-		console.log("woof")
+	purr(){
+		console.log("purr")
 	}
 }
 
